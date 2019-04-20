3de0700d0bf9946d83d5d5ea2a89773701ac9a70
Joe <joe.vu@homeslicesolutions.com>
2019-04-20 11:33:42 -0700

    Enhanced with isLoading flag and callback events

--- addon/index.js
+++ addon/index.js
@@ -8,19 +8,68 @@ export default Component.extend({
 
   tagName: '',
 
-  modelName:   '',
-  recordId:    null,
+  /**
+   * Name of the model type to be fetched
+   * i.e. 'user'
+   * @type {String}
+   */
+  modelName: '',
+
+  /**
+   * Supporting singular record fetches by a specifying record ID
+   * i.e. GET /users/1 => (recordId: 1)
+   * @type {String | null}
+   */
+  recordId: null,
+
+  /**
+   * Specifying the exact store method to be used to get the data
+   * @type {String:[query, queryRecord, findRecord, findAll, peekAll, peekRecord] | null}
+   */
   storeMethod: null,
-  query:       null,
-  options:     null,
-  throwError:  false,
+
+  /**
+   * Query Parameters for "query" and "queryRecord" methods
+   * @type {Object | null}
+   */
+  queryParams: null,
+
+  /**
+   * Options for the store method
+   * "Optional, may include 'adapterOptions' hash which will be passed to adapter.query"
+   * @type {Object | null}
+   */
+  options: null,
+
+  /**
+   * Flag to either throw the error or just log the error into console if the call fails
+   * False will just console.error the error into the console but will not halt the application
+   * True will halt the application like it normally does
+   * @type {Boolean}
+   */
+  throwError: false,
+
+  /**
+   * Callback for data 
+   * @param {Object:DS.Model}
+   */
+  onSuccess: (/* data */) => {},
+
+   /**
+   * Callback for errors 
+   * @param {Object:Error}
+   */
+  onError: (/* error */) => {},
 
   isLoading: false,
   data:      computed(function() { return !this.recordId ? [] : null; }),
   errors:    computed(function() { return []; }),
 
   /**
-   * Returning the inferred store method by query and record ID Properties
+   * Inferring the correct method to use based on properties that are passed through
+   * this component. Basing on the query and recordId property, the component will
+   * infer to use the assumed correct method matching it's signature. This property
+   * will be used if no storeMethod property is specified
    * @returns {String:[query,queryRecord,findRecord,findAll]}
    */
   inferredStoreMethod: computed('query,recordId', function() {
@@ -43,10 +92,10 @@ export default Component.extend({
       // Set loading
       this.set('isLoading', true);
 
-      // Store Method
+      // Store Method (choose declared method or infer the method)
       const storeMethod = this.storeMethod || this.inferredStoreMethod;
   
-      // Format correct method signature if they have a "recordId"
+      // Format correct method signature based on all properties
       const args = [this.modelName];
       if (this.recordId) args.push(this.recordId);
       if (this.query) args.push(this.query);
@@ -55,8 +104,8 @@ export default Component.extend({
       // Dispatch fetch action
       try {
         const data = await this.store[storeMethod](...args);
-        
         this.set('data', data);
+        this.onSuccess(data);
       } catch(e) {
         this.set('errors', e.errors || e);
 
@@ -66,6 +115,8 @@ export default Component.extend({
           /* eslint no-console: 0 */
           console.error('DataProviderError', e);
         }
+
+        this.onError(e);
       }
       
       this.set('isLoading', false);
@@ -73,15 +124,12 @@ export default Component.extend({
   },
 
   layout: hbs`
-    {{#if isLoading}}
-      Loading...
-    {{else}}
-      {{yield (hash 
-        data=data
-        errors=errors
-        reload=(action "fetch")
-      )}}
-    {{/if}}
+    {{yield (hash
+      isLoading=isLoading
+      data=data
+      errors=errors
+      reload=(action "fetch")
+    )}}
   `
 }).reopenClass({
   positionalParams: ['modelName', 'recordId'],
